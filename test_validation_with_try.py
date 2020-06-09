009b6a54942018120fafc75fb0e81f7e9678e8c3
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 11:23:46 -0500

    Add files via upload

--- test_validation_with_try.py
+++ test_validation_with_try.py
@@ -8,6 +8,9 @@ class MyTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             average(90,- 89, 78)
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            average(90, 89, -78)
 
 
 if __name__ == '__main__':
