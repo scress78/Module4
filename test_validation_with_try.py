7dafd382b686b3344bbc7123c3a0a88ca6287127
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 11:14:16 -0500

    Add files via upload

---
+++ test_validation_with_try.py
@@ -0,0 +1,11 @@
+import unittest
+from input_validation.validation_with_try import average
+
+class MyTestCase(unittest.TestCase):
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            average(-90, 89, 78)
+
+
+if __name__ == '__main__':
+    unittest.main()
