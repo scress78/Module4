92e3aa49c37123d5550c6a16b81d77815d1af0bf
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 01:18:38 -0500

    Add files via upload

---
+++ coupon_calculations.py
@@ -0,0 +1,33 @@
+"""
+Program: coupon_calculations.py
+Author: Spencer Cress
+Date: 6/8
+
+Function that calculates and returns price for Nestedif Assignment
+"""
+
+def calculate_price(price, cash_coupon, percent_coupon):
+    shipping = 0
+    tax = 1.06
+    priceoff = price - cash_coupon
+    print(priceoff)
+    discount = priceoff * (1-percent_coupon)
+    print(discount)
+    if price <= 10:
+        shipping = 5.95
+    elif 10 < price <= 30:
+        shipping = 7.95
+    elif 30 < price <= 50:
+        shipping = 11.95
+    elif price > 50:
+        shipping = 0
+    print(shipping)
+    post_shipping = discount + shipping
+    print(post_shipping)
+    final_price = post_shipping * tax
+    print(final_price)
+    return final_price
+
+if __name__ == '__main__':
+    print_price = calculate_price(7.99,5,.1)
+    print(print_price)
---
+++ text_coupon_calculations.py
@@ -0,0 +1,27 @@
+"""
+Program: test_coupon_calculations.py
+Author: Spencer Cress
+Date: 6/8
+
+This program tests the coupon codes for this weeks assignment
+"""
+
+from store import coupon_calculations as cc
+import unittest
+import unittest.mock as mock
+
+
+class MyTestCase(unittest.TestCase):
+    #def calculate_price(price, cash_coupon, percent_coupon):
+    def test_price_under_10(self):
+        value_under_ten = 7.99
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, .1), 9.16 , places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, .15), 9.00 , places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 5, .2), 8.84 , places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 10, .1), 4.39 , places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 10, .15), 4.5 , places=4)
+        self.assertAlmostEqual(cc.calculate_price(7.99, 10, .20), 4.34, places=4)
+
+if __name__ == '__main__':
+    unittest.main()
+
