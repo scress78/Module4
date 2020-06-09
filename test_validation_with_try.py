c0bd5eb449bfaa27f7a58755641ab16c747a775a
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 11:33:58 -0500

    Add files via upload

--- test_validation_with_try.py
+++ test_validation_with_try.py
@@ -1,3 +1,11 @@
+"""
+Program: test_validation_with_try.py
+Author: Spencer Cress
+Date: 6/9
+
+This program tests validation with try for a module 4 assignement
+"""
+
 import unittest
 from input_validation.validation_with_try import average
 
--- validation_with_try.py
+++ validation_with_try.py
@@ -6,15 +6,20 @@ Date: 6/9
 Student average scores function for assignment
 """
 
+
 def average(score1, score2, score3):
-    if score1 < 0:
-        raise ValueError
-    if score2 < 0:
-        raise ValueError
-    if score3 < 0:
-        raise ValueError
+    try:
+        if score1 < 0:
+            raise ValueError
+        if score2 < 0:
+            raise ValueError
+        if score3 < 0:
+            raise ValueError
+    except:
+        print("A value entered is negative, please select valid input.")
     NUMBER_TESTS = 3
     return float((score1 + score2 + score3)/NUMBER_TESTS)
+
 
 if __name__ == '__main__':
     print(average(-90,82,3))
