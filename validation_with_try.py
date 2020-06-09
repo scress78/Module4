ab8bf923915cbc43b0868228f29d31cd63dd40fc
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 11:26:29 -0500

    Add files via upload

--- validation_with_try.py
+++ validation_with_try.py
@@ -1,7 +1,17 @@
+"""
+Program: validation_with_try.py
+Author: Spencer Cress
+Date: 6/9
+
+Student average scores function for assignment
+"""
+
 def average(score1, score2, score3):
     if score1 < 0:
         raise ValueError
     if score2 < 0:
+        raise ValueError
+    if score3 < 0:
         raise ValueError
     NUMBER_TESTS = 3
     return float((score1 + score2 + score3)/NUMBER_TESTS)
