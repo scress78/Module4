cccf9b0a5c21a663b6d8a45a100e62bc76dd4301
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-09 11:19:57 -0500

    Add files via upload

---
+++ validation_with_try.py
@@ -0,0 +1,8 @@
+def average(score1, score2, score3):
+    if score1 < 0:
+        raise ValueError
+    NUMBER_TESTS = 3
+    return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+if __name__ == '__main__':
+    print(average(-90,82,3))
